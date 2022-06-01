e4d3af789c1b0f01284da7c140e7bc22cfa2843d
Stefanos Petrakis <stefanos.petrakis@gmail.com>
2022-06-01 17:08:13 +0200

    typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,4 +3,4 @@
 node /usr/bin/lt --port 49156 --local-host host.docker.internal > /tmp/sth &
 sleep 3
 export LT_URL=$(cat /tmp/sth | grep -o https:.*)
-echo "::set-output name=lt_url::$LT_URL"
+echo "::set-output name=lt-url::$LT_URL"
