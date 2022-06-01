e9641dd4074d4da1b679342677ff8c618ad30305
Stefanos Petrakis <stefanos.petrakis@gmail.com>
2022-06-01 17:28:06 +0200

    using 172.17.0.1

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -l
 
-node /usr/bin/lt --port 49156 --local-host host.docker.internal > /tmp/sth &
+node /usr/bin/lt --port $1 --local-host $2 > /tmp/sth &
 sleep 3
 export LT_URL=$(cat /tmp/sth | grep -o https:.*)
 echo "::set-output name=lt-url::$LT_URL"
