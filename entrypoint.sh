7b3e8ca743b5dc0438e50b36cd1eba47b08b15b0
Stefanos Petrakis <stefanos.petrakis@gmail.com>
2022-06-01 17:46:22 +0200

    sth

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,5 @@ node /usr/bin/lt --port $1 --local-host $2 > /tmp/sth &
 sleep 3
 export LT_URL=$(cat /tmp/sth | grep -o https:.*)
 echo "::set-output name=lt-url::$LT_URL"
-fg
+# keep alive
+tail -f /dev/null
